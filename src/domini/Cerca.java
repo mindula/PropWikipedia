139f8eb386fe3425994ab41ab23b3cc49e892073
ricard <gascons1995@gmail.com>
2015-04-18 13:47:10 +0200

    Classe Cerca

--- src/domini/Cerca.java
+++ src/domini/Cerca.java
@@ -2,10 +2,18 @@ package domini;
 
 import org.grupwiki.graf.Graf;
 
+import java.util.ArrayList;
 import java.util.Date;
 
 public class Cerca {
-    //falta implementar la composicio
     private String query;
     private Date data;
+
+    public static ArrayList<> cercarCategoria(String query, Date data) {
+
+    }
+
+    private String BFS() {
+
+    }
 }
