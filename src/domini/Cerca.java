bbef218887b1de7de84c0b33b9ff56452ad39140
ricard <gascons1995@gmail.com>
2015-04-20 20:46:13 +0200

    Cerca + InfoCerca

--- src/domini/Cerca.java
+++ src/domini/Cerca.java
@@ -2,20 +2,24 @@ package domini;
 
 import graf.GrafWikipedia;
 import org.grupwiki.graf.Arc;
+
+import javax.xml.soap.Node;
+import java.text.*;
 import java.util.*;
 import java.util.regex.Pattern;
 
 public abstract class Cerca {
 
-    static Pattern patro = Pattern.compile(Pattern.quote(a.getNodeB().getNom()),
-            Pattern.CASE_INSENSITIVE);
-    public static ArrayList<NodeWiki> cercarCategoria(GrafWikipedia<NodeWiki> g, String query, Date data) {
-        //Encapsular query, data i ArrayList dins un objecte?
-        ArrayList<NodeWiki> res = BFS(g, query);
-        return res;
+    /*static Pattern patro = Pattern.compile(Pattern.quote(a.getNodeB().getNom()),
+            Pattern.CASE_INSENSITIVE);*/
+    public static InfoCerca cercarCategoria(GrafWikipedia<NodeWiki> g, String query) {
+        DateFormat dateFormat = new SimpleDateFormat("dd/MM/yyyy");
+        Date date = new Date();
+        ArrayList<NodeWiki> res = cercaGraf(g, query);
+        return new InfoCerca(res, dateFormat.format(date));
     }
 
-    private static ArrayList<NodeWiki> BFS(GrafWikipedia<NodeWiki> g, String query) {
+    /*private static ArrayList<NodeWiki> BFS(GrafWikipedia<NodeWiki> g, String query) {
         ArrayList<NodeWiki> res = new ArrayList<NodeWiki>();
         Queue<NodeWiki> q = new LinkedList<NodeWiki>();
         Queue<NodeWiki> visited = new LinkedList<NodeWiki>();       //hi ha formes més eficients de fer-ho
@@ -35,23 +39,24 @@ public abstract class Cerca {
             }
         }
         return res;
-    }
+    }*/
 
-    private static Queue<NodeWiki> cercaGraf (GrafWikipedia<NodeWiki> g, String query) {
-        PriorityQueue<NodeWiki> pq = new PriorityQueue<NodeWiki>(g.ordre(), new Comparator<NodeWiki>() {
-            @Override
-            public int compare(NodeWiki nodeWiki, NodeWiki t1) {
-                return nodeWiki.getNom().in
-                return 0;
-            }
-        });
+    private static ArrayList<NodeWiki> cercaGraf (GrafWikipedia<NodeWiki> g, String query) {
+        ArrayList<NodeWiki> pq = new ArrayList<NodeWiki>();
+        Set<NodeWiki> s = g.getNodes();
+        for (NodeWiki n : s) {
+                boolean match = Pattern.compile(Pattern.quote(n.getNom()),
+                        Pattern.CASE_INSENSITIVE).matcher(query).find();
+                if (match) pq.add(n);
+        }
+        return pq;
     }
 
     private static NodeWiki getRandomNode(Set<NodeWiki> s) {
         int size = s.size();
         int item = new Random().nextInt(size);
         int i = 0;
-        for(NodeWiki node : s) {
+        for (NodeWiki node : s) {
             if (i == item)
                 return node;
             ++i;
---
+++ src/domini/InfoCerca.java
@@ -0,0 +1,21 @@
+package domini;
+
+import java.util.ArrayList;
+
+public class InfoCerca {
+    private ArrayList<NodeWiki> a;
+    private String data;
+
+    public InfoCerca(ArrayList<NodeWiki> a, String data) {
+        this.a = a;
+        this.data = data;
+    }
+
+    public ArrayList<NodeWiki> getResultats() {
+        return a;
+    }
+
+    public String getDataCerca() {
+        return data;
+    }
+}
