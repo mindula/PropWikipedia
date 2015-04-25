8faf367d954910bf3ad87bbfef29aa6f36205265
ricard <gascons1995@gmail.com>
2015-04-25 17:41:54 +0200

    Funció m2

--- src/domini/AlgorismeLouvain.java
+++ src/domini/AlgorismeLouvain.java
@@ -5,29 +5,32 @@ import org.grupwiki.graf.Arc;
 import org.grupwiki.graf.ConjuntComunitats;
 import org.grupwiki.graf.Graf;
 
+import java.util.List;
 import java.util.Set;
 
-public class AlgorismeLouvain extends Algoritme<NodeWiki>{
+public class AlgorismeLouvain<T> extends Algoritme<T>{
+
     @Override
-    public ConjuntComunitats<NodeWiki> cercarComunitats(Graf<NodeWiki> graf, int k, int l) throws Exception{
-        int m2 = m2(graf);
+    public ConjuntComunitats<T> cercarComunitats(Graf<T> graf, int k, int l) throws Exception{
+        double m2 = m2(graf);
         // TODO: implementar l'algoritme
     }
 
     // Calcula m*2, es a dir, per a tot arc del graf, la suma del seu pes (un arc del node A al node B s'ha de sumar 2 cops)
-    private int m2(Graf<NodeWiki> graf){
-        int sumaArcs = 0;
-        // TODO: falta metode getArcs del graf! O necessitem accedir a l'atribut privat de la classe Graf...
-        // TODO: l'alternativa amb l'especificacio actual seria fer un getNodes i un getNodesAdjacents per cada un, pero es poc eficient
-            //DONE: un cop Alicia ho permeti, tenim una operació amb cost O(1)
-        return sumaArcs;
+    private double m2(Graf<T> graf){
+        double sumaArcs = 0;
+        List<Arc<T>> arcs = graf.getArcs();
+        for (Arc<T> a : arcs) {
+            sumaArcs += a.getPes();
+        }
+        return sumaArcs*2;
     }
 
     // Calcula ki: la suma dels pesos dels arcs incidents al NodeWiki node
-    private int ki(Graf<NodeWiki> graf, NodeWiki node){
+    private int ki(Graf<T> graf, T node){
         int ki = 0;
-        Set<Arc<NodeWiki>> set = graf.getNodesAdjacents(node);
-        for(Arc<NodeWiki> a : set) ki += a.getPes();
+        Set<Arc<T>> set = graf.getNodesAdjacents(node);
+        for(Arc<T> a : set) ki += a.getPes();
         return ki;
     }
 
