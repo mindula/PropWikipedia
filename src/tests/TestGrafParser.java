8212c252dc738dd9e26abfd72bb60de40419ed8b
Aleixpb2 <aleixpb2@users.noreply.github.com>
2015-05-03 18:01:57 +0200

    Tests

--- src/tests/TestGrafParser.java
+++ src/tests/TestGrafParser.java
@@ -16,42 +16,41 @@ import java.util.Scanner;
 
 public class TestGrafParser {
     public static void main (String[] args) {
-        System.out.println("Escriu el numero del test o el path del fitxer:");
-        System.out.println("test 0: misc/cats_test.txt");
-        System.out.println("test 1: misc/cats_small_test.txt");
-        System.out.println("test 2: misc/cats.txt (fitxer molt gran)");
         Scanner sc = new Scanner(System.in);
-        String path = sc.nextLine();
-        switch(Integer.parseInt(path)){
+        System.out.println("Escriu 0 per provar un test per defecte, 1 per un test predeterminat i 2 per sortir");
+        int opcio = sc.nextInt();
+        if(opcio == 2) return;
+        String path = "";
+        switch(opcio) {
             case 0:
-                path = "misc/cats_test.txt";
+                System.out.println("Escriu el numero del test:");
+                System.out.println("test 0: misc/cats_test.txt");
+                System.out.println("test 1: misc/cats_small_test.txt");
+                System.out.println("test 2: misc/cats.txt (fitxer molt gran)");
+                opcio = sc.nextInt();
+                switch (opcio) {
+                    case 0:
+                        path = "misc/cats_test.txt";
+                        break;
+                    case 1:
+                        path = "misc/cats_small_test.txt";
+                        break;
+                    case 2:
+                        path = "misc/cats.txt";
+                        break;
+                }
                 break;
             case 1:
-                path = "misc/cats_small_test.txt";
+                System.out.println("Escriu el path del fitxer:");
+                path = sc.nextLine();
                 break;
-            case 2:
-                path = "misc/cats.txt";
-                break;
-            default:
         }
-        GrafWikipedia g = Sessio.getInstance().getGrafWiki();
-        // Test extra:
-        /*
-        NodePagina nodep1 = new NodePagina("TESTP1");
-        NodePagina nodep2 = new NodePagina("TESTP2");
-        NodeCategoria nodec1 = new NodeCategoria("TESTC1");
-        g.afegirNode(nodep1);
-        g.afegirNode(nodep2);
-        g.afegirNode(nodec1);
-        g.afegirArcPC(nodep1, nodec1);
-        g.afegirArcPC(nodep2, nodec1);
-        g.afegirArcPC(nodep1, nodec1);
-        */
 
+        GrafWikipedia g = Sessio.getInstance().getGrafWiki();
         GrafParser grafParser = new GrafParser(g);
         grafParser.parse(path);
         System.out.println("Vols mostrar el graf resultat? 1 - Si, 0 - No");
-        int opcio = sc.nextInt();
-        if(opcio == 1) System.out.println(g);
+        opcio = sc.nextInt();
+        if (opcio == 1) System.out.println(g);
     }
 }
