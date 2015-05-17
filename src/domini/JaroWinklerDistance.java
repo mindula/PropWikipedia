d6c68cf3eec5d872f645f8fd27e2e9ecf389da14
aramirr <eduardcasellas18@gmail.com>
2015-05-17 22:32:52 +0200

    jaro_winkler distance

--- src/domini/JaroWinklerDistance.java
+++ src/domini/JaroWinklerDistance.java
@@ -22,21 +22,41 @@ public class JaroWinklerDistance {
         int m = 0;
         int t = 0;
         int l = 0;
+        int w = 0;
         boolean fiPrefix = false;
         for (int i = 0; i < s1.length(); ++i){
             char c = s1.charAt(i);
+
             for (int j = Math.max(0, i - dmax); j < Math.min(s2.length(), i+dmax); ++j){
                 if (c == s2.charAt(j)){
                     ++m;
-                    if (i != j) ++t;
+                    if (i != j ){
+                        ++t;
+                        if (j < s1.length()){
+                            if (s2.charAt(i) == s1.charAt(j)) {
+                                ++w;
+                            }
+                        }
+                    }
                     else if (!fiPrefix) ++l;
+                    break;
                 }
                 else if(i == j && !fiPrefix) fiPrefix = true;
             }
         }
+        l = Math.min (4, l);
         if (m == 0) return 0.0;
-        t /= 2;
+        System.out.println(m);
+        System.out.println(t);
+        w /= 2;
+        t -= w;
+        System.out.println(t);
+        int x = m-t;
+        System.out.println(m/(double)s1.length());
+        System.out.println(m/(double)s2.length());
+        System.out.println(x);
         double dj = ((m/(double)s1.length()) + (m/(double)s2.length()) + ((m-t)/(double)m))/3;
+        System.out.println(dj);
         if (dj < 0.7) return dj;
         return dj+(l*0.1*(1-dj));
     }
