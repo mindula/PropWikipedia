10d1bd6c2153d3e4759f98539e4e0209d77abf35
Agusti Bau <agustibau@gmail.com>
2015-05-17 19:00:13 +0200

    limpiesa

--- src/graf/grafgenerator/Criteris/CriteriNom.java
+++ src/graf/grafgenerator/Criteris/CriteriNom.java
@@ -1,7 +1,6 @@
 package graf.grafgenerator.Criteris;
 
 import domini.JaroWinklerDistance;
-import domini.LevenshteinDistance;
 import graf.GrafWikipedia;
 import graf.NodeCategoria;
 
@@ -16,23 +15,15 @@ import graf.NodeCategoria;
  */
 public class CriteriNom extends Criteri{
 
-    private final double maximGrauDiferencia;
 
-    public CriteriNom(double ponderacio, double maximGrauDiferencia) {
+    public CriteriNom(double ponderacio) {
         super(ponderacio);
-        this.maximGrauDiferencia = maximGrauDiferencia;
     }
 
     @Override
     public double getPes(NodeCategoria n1, NodeCategoria n2, GrafWikipedia graf) {
         return JaroWinklerDistance.calculate(n1.getNom(), n2.getNom());
 
-        /*int maximCost = (int) (Math.max(n1.getNom().length(), n2.getNom().length()) * maximGrauDiferencia);
-
-        if (cost >  maximCost)
-            return 0;
-        else*/
-            //return cost;
     }
 
     @Override
