86881174c6d518a0809b7fa1179dbe4753934124
ricard <gascons1995@gmail.com>
2015-05-22 15:51:46 +0200

    navegar

--- src/presentacio/MainWindow.java
+++ src/presentacio/MainWindow.java
@@ -25,24 +25,13 @@ public class MainWindow extends javax.swing.JFrame {
     private void initComponents() {
 
         jTabbedPane1 = new javax.swing.JTabbedPane();
-        jPanel1 = new javax.swing.JPanel();
+        jPanel1 = new Navegacio();
         jPanel3 = new javax.swing.JPanel();
         jPanel2 = new javax.swing.JPanel();
         temes2 = new Temes();
         carregar1 = new Carregar();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
-
-        javax.swing.GroupLayout jPanel1Layout = new javax.swing.GroupLayout(jPanel1);
-        jPanel1.setLayout(jPanel1Layout);
-        jPanel1Layout.setHorizontalGroup(
-                jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-                        .addGap(0, 971, Short.MAX_VALUE)
-        );
-        jPanel1Layout.setVerticalGroup(
-                jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-                        .addGap(0, 671, Short.MAX_VALUE)
-        );
 
         jTabbedPane1.addTab("Navegar", jPanel1);
 
@@ -128,7 +117,7 @@ public class MainWindow extends javax.swing.JFrame {
 
     // Variables declaration - do not modify
     private Carregar carregar1;
-    private javax.swing.JPanel jPanel1;
+    private Navegacio jPanel1;
     private javax.swing.JPanel jPanel2;
     private javax.swing.JPanel jPanel3;
     private javax.swing.JTabbedPane jTabbedPane1;
