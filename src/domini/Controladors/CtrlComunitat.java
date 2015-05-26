00bcd424368553333d87489b7a744996fe38d543
eduard.casellas <eduardcasellas18@gmail.com>
2015-05-26 17:36:12 +0200

    CtrlComunitat

--- src/domini/Controladors/CtrlComunitat.java
+++ src/domini/Controladors/CtrlComunitat.java
@@ -2,7 +2,11 @@ package domini.Controladors;
 
 import domini.ComunitatWiki;
 import domini.ConjuntComunitatWiki;
+import graf.GrafWikipedia;
+import graf.NodeCategoria;
 import graf.NodeWiki;
+import graf.OperacionsConjunts;
+import prop.classescompartides.graf.Comunitat;
 import prop.classescompartides.graf.ConjuntComunitats;
 
 /**
@@ -12,13 +16,15 @@ import prop.classescompartides.graf.ConjuntComunitats;
  */
 public class CtrlComunitat {
     public ConjuntComunitatWiki conjunt;
+    public GrafWikipedia graf;
 
     public CtrlComunitat(){
         this.conjunt = new ConjuntComunitatWiki();
+        this.graf = CtrlWikipedia.getInstance().getGrafWiki();
     }
 
     /**
-     * Cas d'us crea tema
+     * Cas d'us Crear tema.
      */
     public void creaComunitat(String nom){
         ComunitatWiki c = new ComunitatWiki();
@@ -26,12 +32,64 @@ public class CtrlComunitat {
         conjunt.afegirComunitat(c);
     }
 
+    /**
+     * Cas d'us Modificar tema. Canviar nom.
+     */
     public void modNomComunitat(int id, String nomnou) throws  Exception{
         ComunitatWiki c = (ComunitatWiki) conjunt.getComunitats().get(id);
         c.setNom(nomnou);
     }
 
+    /**
+     * Cas d'us Modificar tema. Afegir Categoria.
+     */
+    public void afegirCatComunitat (int idComunitat, String nomCategoria) throws Exception {
+        NodeCategoria categoria = graf.getNodeCat(nomCategoria);
+        conjunt.getComunitat(idComunitat).afegirNode(categoria);
+    }
 
+    /**
+     * Cas d'us Modificar tema. Eliminar Categoria.
+     */
+    public void eliminarCatComunitat(int idComunitat, String nomCategoria) throws Exception {
+        NodeCategoria categoria = graf.getNodeCat(nomCategoria);
+        conjunt.getComunitat(idComunitat).eliminarNode(categoria);
+    }
 
+    /**
+     * Cas d'us Consultar tema.
+     */
+    public ConjuntComunitatWiki getConjunt() {
+        return conjunt;
+    }
 
+    /**
+     * Cas d'us Operació entre temes. Unio.
+     */
+    public Comunitat<NodeCategoria> unio (int idComunitat1, int idComunitat2) throws Exception{
+        return OperacionsConjunts.unio(conjunt.getComunitat(idComunitat1), conjunt.getComunitat(idComunitat2));
+    }
+
+    /**
+     * Cas d'us Operació entre temes. Interseccio.
+     */
+    public Comunitat<NodeCategoria> interseccio (int idComunitat1, int idComunitat2) throws Exception{
+        return OperacionsConjunts.interseccio(conjunt.getComunitat(idComunitat1), conjunt.getComunitat(idComunitat2));
+    }
+
+    /**
+     * Cas d'us Operació entre temes. Diferencia.
+     */
+    public Comunitat<NodeCategoria> diferencia (int idComunitat1, int idComunitat2) throws Exception{
+        return OperacionsConjunts.diferencia(conjunt.getComunitat(idComunitat1), conjunt.getComunitat(idComunitat2));
+    }
+
+    /**
+     * Cas d'us Eliminar comunitat.
+     */
+    public void eliminarComunitat (int idComunitat) throws Exception {
+        conjunt.eliminarComunitat(conjunt.getComunitat(idComunitat));
+    }
+
+    //TODO: Metode OPCIONAL Assignar color a un tema
 }
