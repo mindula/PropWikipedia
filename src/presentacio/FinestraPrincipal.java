c2ebe5b607364f6547300fc7bffab3d104db9a1f
ricard <gascons1995@gmail.com>
2015-05-28 15:58:48 +0200

    first commit

--- src/presentacio/FinestraPrincipal.java
+++ src/presentacio/FinestraPrincipal.java
@@ -1,9 +1,30 @@
 package presentacio;
 
+import javafx.application.Application;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.layout.StackPane;
+import javafx.stage.Stage;
+
 /**
  * Grup 3: Wikipedia
- * Usuari: ricard
- * Data: 5/28/15
+ * Usuari: ricard.gascons
+ * Data: 28/5/15
  */
-public class FinestraPrincipal {
+public class FinestraPrincipal extends Application {
+
+    @Override
+    public void start(Stage stage) throws Exception {
+        stage.setTitle("Wikipedia");
+
+        StackPane root = new StackPane();
+        stage.setScene(new Scene(root, 1024, 720));
+        stage.show();
+    }
+
+    public static void main(String[] args) {
+        launch(args);
+    }
 }
