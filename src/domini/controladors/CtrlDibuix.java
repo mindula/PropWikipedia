93203eaee66b9b54a902cb1c3362af1bee1cabb3
eduard.casellas <eduardcasellas18@gmail.com>
2015-05-29 19:46:40 +0200

    CtrlDibuix

--- src/domini/controladors/CtrlDibuix.java
+++ src/domini/controladors/CtrlDibuix.java
@@ -13,14 +13,30 @@ import prop.classescompartides.graf.Comunitat;
  * Data: 29/05/15
  */
 public class CtrlDibuix {
-    private String[] color = {"red", "blue", "yellow", "brown", "purple"};
+    private String[] color = {"rgb(0,0,0)", "rgb(51,51,51)", "rgb(255,0,0)", "rgb(0,255,0)", "rgb(0,0,255)", "rgb(255,255,0)", "rgb(0,255,255)", "rgb(255,0,255)"};
+
 
     public void DibuixarGraf(){
         Graph graf = new SingleGraph("Graf");
         graf.setStrict(false);
         for(NodeCategoria cat: CtrlWikipedia.getInstance().getGrafWiki().getCategories()){
             graf.addNode(cat.getNom());
-            graf.getNode(cat.getNom()).addAttribute("ui.style", "fill-color: rgb(0,100,255);");
+        }
+        int i = 0;
+        for (Arc<NodeCategoria> arc:  CtrlWikipedia.getInstance().getGrafWiki().getArcs())  {
+            graf.addEdge(String.valueOf(i),arc.getNodeA().getNom(),arc.getNodeB().getNom());
+            ++i;
+        }
+
+        graf.display();
+    }
+
+
+    public void DibuixarGrafAmbComunitats(){
+        Graph graf = new SingleGraph("Graf");
+        graf.setStrict(false);
+        for(NodeCategoria cat: CtrlWikipedia.getInstance().getGrafWiki().getCategories()){
+            graf.addNode(cat.getNom());
         }
         int i = 0;
         for (Arc<NodeCategoria> arc:  CtrlWikipedia.getInstance().getGrafWiki().getArcs())  {
