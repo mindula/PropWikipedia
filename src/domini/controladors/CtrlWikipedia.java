a11fa53552a5602ff756f0bc1ec4a23fbd2b65bc
ricard <gascons1995@gmail.com>
2015-06-01 00:09:40 +0200

    Operacions cjts

--- src/domini/controladors/CtrlWikipedia.java
+++ src/domini/controladors/CtrlWikipedia.java
@@ -39,7 +39,6 @@ public class CtrlWikipedia implements Serializable{
         grafWiki = new GrafWikipedia();
         grafAlgoritme = new Graf<>();
         conjuntsGenerats = new ConjuntComunitats<>();
-
     }
 
     /**
